2c2a404f15553fe90f2a89aa16320a34bcb0178b
Ermat <ealymbaev@gmail.com>
2021-10-11 18:34:13 +0600

    Make all fields optional in MarketInfo

--- MarketKit/Classes/Models/MarketInfo.swift
+++ MarketKit/Classes/Models/MarketInfo.swift
@@ -2,9 +2,9 @@ import Foundation
 
 public struct MarketInfo {
     public let fullCoin: FullCoin
-    public let price: Decimal
+    public let price: Decimal?
     public let priceChange: Decimal?
-    public let marketCap: Decimal
+    public let marketCap: Decimal?
     public let totalVolume: Decimal?
 }
 
