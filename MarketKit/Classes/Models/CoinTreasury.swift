ed1ddd3596d100f5081af26263ef51c9c3755577
Ermat <ealymbaev@gmail.com>
2021-11-04 16:09:37 +0600

    Add `logo` to `CoinTreasury`

--- MarketKit/Classes/Models/CoinTreasury.swift
+++ MarketKit/Classes/Models/CoinTreasury.swift
@@ -4,6 +4,7 @@ import ObjectMapper
 public class CoinTreasury: ImmutableMappable {
     public let type: TreasuryType
     public let fund: String
+    public let logo: String
     public let amount: Decimal
     public let amountInCurrency: Decimal
     public let country: String
@@ -11,6 +12,7 @@ public class CoinTreasury: ImmutableMappable {
     required public init(map: Map) throws {
         type = try map.value("type")
         fund = try map.value("fund")
+        logo = try map.value("logo")
         amount = try map.value("amount", using: Transform.stringToDecimalTransform)
         amountInCurrency = try map.value("amount_in_currency", using: Transform.stringToDecimalTransform)
         country = try map.value("country")
