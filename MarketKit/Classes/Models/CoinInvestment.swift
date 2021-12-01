4ee53682dd2492eb24907fddea834cf31f386a6e
Ermat <ealymbaev@gmail.com>
2021-12-01 18:29:02 +0600

    CoinInvestment: remove `amountInCurrency` field

--- MarketKit/Classes/Models/CoinInvestment.swift
+++ MarketKit/Classes/Models/CoinInvestment.swift
@@ -5,14 +5,12 @@ public class CoinInvestment: ImmutableMappable {
     public let date: Date
     public let round: String
     public let amount: Decimal
-    public let amountInCurrency: Decimal
     public let funds: [Fund]
 
     required public init(map: Map) throws {
         date = try map.value("date", using: Transform.stringToDateTransform)
         round = try map.value("round")
         amount = try map.value("amount", using: Transform.stringToDecimalTransform)
-        amountInCurrency = try map.value("amount_in_currency", using: Transform.stringToDecimalTransform)
         funds = try map.value("funds")
     }
 
