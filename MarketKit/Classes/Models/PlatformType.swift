27cdd771f82329cec24fc32ab1b3dc6fabcd0f20
Ermat <ealymbaev@gmail.com>
2022-02-24 12:13:42 +0600

    Add `polygonPos` platform type

--- MarketKit/Classes/Models/PlatformType.swift
+++ MarketKit/Classes/Models/PlatformType.swift
@@ -1,11 +1,13 @@
 public enum PlatformType {
     case ethereum
     case binanceSmartChain
+    case polygonPos
 
     public var coinTypeIdPrefixes: [String] {
         switch self {
         case .ethereum: return ["ethereum", "erc20"]
         case .binanceSmartChain: return ["binanceSmartChain", "bep20"]
+        case .polygonPos: return ["polygonPos"]
         }
     }
 }
