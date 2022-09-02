1a0eadb3c16ae7a590933843d699cf0d9c37c712
Ermat <ealymbaev@gmail.com>
2022-09-02 18:12:33 +0600

    Minor fix for `MarketOverviewResponse`

--- MarketKit/Classes/Models/MarketOverviewResponse.swift
+++ MarketKit/Classes/Models/MarketOverviewResponse.swift
@@ -12,8 +12,8 @@ struct MarketOverviewResponse: ImmutableMappable {
         globalMarketPoints = try map.value("global")
         coinCategories = try map.value("sectors")
         topPlatforms = try map.value("platforms")
-        collections1d = try map.value("nft.one_day")
-        collections1w = try map.value("nft.seven_day")
-        collections1m = try map.value("nft.thirty_day")
+        collections1d = (try? map.value("nft.one_day")) ?? []
+        collections1w = (try? map.value("nft.seven_day")) ?? []
+        collections1m = (try? map.value("nft.thirty_day")) ?? []
     }
 }
