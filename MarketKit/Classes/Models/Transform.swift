53526d3ab460bbf892d0bc7671453c5bf9b15799
Ermat <ealymbaev@gmail.com>
2022-09-02 12:19:36 +0600

    Make `Transform` helper public

--- MarketKit/Classes/Models/Transform.swift
+++ MarketKit/Classes/Models/Transform.swift
@@ -1,7 +1,7 @@
 import Foundation
 import ObjectMapper
 
-struct Transform {
+public struct Transform {
 
     private static let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -9,36 +9,52 @@ struct Transform {
         return formatter
     }()
 
-   static let stringToDecimalTransform: TransformOf<Decimal, String> = TransformOf(fromJSON: { string -> Decimal? in
-       guard let string = string else { return nil }
-       return Decimal(string: string)
-   }, toJSON: { (value: Decimal?) in
-       guard let value = value else { return nil }
-       return value.description
-   })
+    public static let stringToDecimalTransform: TransformOf<Decimal, String> = TransformOf(fromJSON: { string -> Decimal? in
+        guard let string = string else {
+            return nil
+        }
+        return Decimal(string: string)
+    }, toJSON: { (value: Decimal?) in
+        guard let value = value else {
+            return nil
+        }
+        return value.description
+    })
 
-   static let stringToIntTransform: TransformOf<Int, String> = TransformOf(fromJSON: { string -> Int? in
-       guard let string = string else { return nil }
-       return Int(string)
-   }, toJSON: { (value: Int?) in
-       guard let value = value else { return nil }
-       return value.description
-   })
+    public static let stringToIntTransform: TransformOf<Int, String> = TransformOf(fromJSON: { string -> Int? in
+        guard let string = string else {
+            return nil
+        }
+        return Int(string)
+    }, toJSON: { (value: Int?) in
+        guard let value = value else {
+            return nil
+        }
+        return value.description
+    })
 
-   static let doubleToDecimalTransform: TransformOf<Decimal, Double> = TransformOf(fromJSON: { double -> Decimal? in
-       guard let double = double else { return nil }
-       return Decimal(double)
-   }, toJSON: { (value: Decimal?) in
-       guard let value = value else { return nil }
-       return (value as NSDecimalNumber).doubleValue
-   })
+    public static let doubleToDecimalTransform: TransformOf<Decimal, Double> = TransformOf(fromJSON: { double -> Decimal? in
+        guard let double = double else {
+            return nil
+        }
+        return Decimal(double)
+    }, toJSON: { (value: Decimal?) in
+        guard let value = value else {
+            return nil
+        }
+        return (value as NSDecimalNumber).doubleValue
+    })
 
-   static let stringToDateTransform: TransformOf<Date, String> = TransformOf(fromJSON: { string -> Date? in
-       guard let string = string else { return nil }
-       return dateFormatter.date(from: string)
-   }, toJSON: { (value: Date?) in
-       guard let value = value else { return nil }
-       return dateFormatter.string(from: value)
-   })
+    public static let stringToDateTransform: TransformOf<Date, String> = TransformOf(fromJSON: { string -> Date? in
+        guard let string = string else {
+            return nil
+        }
+        return dateFormatter.date(from: string)
+    }, toJSON: { (value: Date?) in
+        guard let value = value else {
+            return nil
+        }
+        return dateFormatter.string(from: value)
+    })
 
 }
