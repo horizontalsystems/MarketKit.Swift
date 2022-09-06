7c53666a18a3e4cb6da3673767404e5040c893a9
ant013 <ant013@mail.ru>
2022-09-06 11:55:46 +0400

    Refactor hashable|equatable functions. Fix 'blockchain' equatable fields

--- MarketKit/Classes/Models/TokenType.swift
+++ MarketKit/Classes/Models/TokenType.swift
@@ -83,7 +83,7 @@ extension TokenType: Equatable {
 
     public static func ==(lhs: TokenType, rhs: TokenType) -> Bool {
         let (lhsType, lhsReference) = lhs.values
-        let (rhsType, rhsReference) = lhs.values
+        let (rhsType, rhsReference) = rhs.values
 
         return lhsType == rhsType && lhsReference == rhsReference
     }
