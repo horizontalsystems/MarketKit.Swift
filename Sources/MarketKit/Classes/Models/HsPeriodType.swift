8126d67cfada711568a65254d7140af76165907a
ant013 <ant013@mail.ru>
2023-06-27 17:50:52 +0600

    Show chart settings module
    - Fix chart touch line
    - add enabled for chartIndicator inside classes
    - add custom period type with additional points for indicators

--- Sources/MarketKit/Classes/Models/HsPeriodType.swift
+++ Sources/MarketKit/Classes/Models/HsPeriodType.swift
@@ -2,9 +2,9 @@ import Foundation
 
 public enum HsPeriodType: Hashable {
     static let keyAll = "all"
-    public static let day1 = Self.byPeriod(.day1)
 
     case byPeriod(HsTimePeriod)
+    case byCustomPoints(HsTimePeriod, Int)
     case byStartTime(TimeInterval)
 
     public init?(rawValue: String) {
@@ -13,14 +13,19 @@ public enum HsPeriodType: Hashable {
             return
         }
         let chunks = rawValue.split(separator: "_")
-        if chunks.count == 2,
-           chunks[0] == Self.keyAll,
-           let timestamp = Int(chunks[1]) {
+        if chunks.count == 2 {
+            if chunks[0] == Self.keyAll,
+               let timestamp = Int(chunks[1]) {
 
-            self = .byStartTime(TimeInterval(timestamp))
-            return
+                self = .byStartTime(TimeInterval(timestamp))
+                return
+            } else if let period = HsTimePeriod(rawValue: String(chunks[0])),
+                      let pointCount = Int(chunks[1]) {
+
+                self = .byCustomPoints(period, pointCount)
+                return
+            }
         }
-
         self = .byPeriod(.day1)
     }
 
@@ -28,13 +33,17 @@ public enum HsPeriodType: Hashable {
         switch self {
         case .byPeriod(let interval): return interval.rawValue
         case .byStartTime(let timeStart): return [Self.keyAll, Int(timeStart).description].joined(separator: "_")
+        case .byCustomPoints(let interval, let pointCount): return [interval.rawValue, pointCount.description].joined(separator: "_")
         }
     }
 
     public func hash(into hasher: inout Hasher) {
         switch self {
-        case .byPeriod(let period): hasher.combine(period)
+        case .byPeriod(let interval): hasher.combine(interval)
         case .byStartTime(let startTime): hasher.combine(startTime)
+        case .byCustomPoints(let interval, let pointCount):
+            hasher.combine(interval)
+            hasher.combine(pointCount)
         }
     }
 
@@ -42,6 +51,7 @@ public enum HsPeriodType: Hashable {
         switch (lhs, rhs) {
         case (.byPeriod(let lhsPeriod), .byPeriod(let rhsPeriod)): return lhsPeriod == rhsPeriod
         case (.byStartTime(let lhsStartTime), .byStartTime(let rhsStartTime)): return lhsStartTime == rhsStartTime
+        case (.byCustomPoints(let lhsI, let lhsC), .byCustomPoints(let rhsI, let rhsC)): return lhsI == rhsI && lhsC == rhsC
         default: return false
         }
     }
